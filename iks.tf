e0c83edf7bad9736e2d71679ae2d5e6dfd6be5e3
Nadine2016 <nspies@us.ibm.com>
2020-03-25 09:14:06 -0400

    Update iks.tf

--- iks.tf
+++ iks.tf
@@ -7,29 +7,29 @@ data ibm_resource_group "resource_group" {
      name = default
    }
    
-resource ibm_container_cluster "schematics" {
-     name            = "schematics"
-     datacenter      = "dal10"
-     machine_type    = "b3c.4x16"
-     hardware        = "shared"
-     public_vlan_id  = "${var.public_vlan_dal10}"
-     private_vlan_id = "${var.private_vlan_dal10}"
+##resource ibm_container_cluster "schematics" {
+##     name            = "schematics"
+##     datacenter      = "dal10"
+##     machine_type    = "b3c.4x16"
+##     hardware        = "shared"
+##     public_vlan_id  = "${var.public_vlan_dal10}"
+##     private_vlan_id = "${var.private_vlan_dal10}"
 
-     kube_version = "1.16"
+##     kube_version = "1.16"
 
-     default_pool_size = 3
+##     default_pool_size = 3
      
-     public_service_endpoint  = "true"
-     private_service_endpoint = "true"
+##     public_service_endpoint  = "true"
+##     private_service_endpoint = "true"
 
-     resource_group_id = "${data.ibm_resource_group.resource_group.id}"
-}
+##     resource_group_id = "${data.ibm_resource_group.resource_group.id}"
+##}
    
-resource ibm_container_worker_pool_zone_attachment "dal12" {
-     cluster         = "mycluster"
-     worker_pool     = "${ibm_container_cluster.schematics.worker_pools.id[0]}"
-     zone            = "dal12"
-     private_vlan_id = "${var.private_vlan_dal12}"
-     public_vlan_id  = "${var.public_vlan_dal12}"
-}
+##resource ibm_container_worker_pool_zone_attachment "dal12" {
+##     cluster         = "mycluster"
+##     worker_pool     = "${ibm_container_cluster.schematics.worker_pools.id[0]}"
+##     zone            = "dal12"
+##     private_vlan_id = "${var.private_vlan_dal12}"
+##     public_vlan_id  = "${var.public_vlan_dal12}"
+##}
    
