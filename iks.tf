51a520a5cc32dc2acd2d48ae866af0925e9b6774
Nadine2016 <nspies@us.ibm.com>
2020-03-25 09:17:54 -0400

    Update iks.tf

--- iks.tf
+++ iks.tf
@@ -3,9 +3,9 @@ variable "public_vlan_dal10" {}
 variable "private_vlan_dal12" {}
 variable "public_vlan_dal12" {}
 
-data ibm_resource_group "resource_group" {
-     name = "default"
-   }
+##data ibm_resource_group "resource_group" {
+##     name = "default"
+##   }
    
 ##resource ibm_container_cluster "schematics" {
 ##     name            = "schematics"
